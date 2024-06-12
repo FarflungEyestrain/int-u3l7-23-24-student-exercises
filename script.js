01653603eb4c268e12f8aadf7f23750ac559ed45
FarflungEyestrain <stedgegavin@gmail.com>
2024-06-12 14:26:24 -0400

    dsds

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // 1. Declare a variable called myName. 
+let myName = "Gavin";
 //  - Give it the value of your name. 
 //  - This is a string so the value should be in quotes ""
 
 
 
 // 2. Declare a variable called age. 
+let age = 18;
 //  - Give it the value of your age. 
 //  - This is a number, so the value does not have to be in quotes
 
 // 3. Declare a variable called answer.
+let answer;
 //  - This should save the div with a class of `answer`
-
 
 // 4. When you're done, remove the comment from the code below.
 
-// answer.innerHTML = myName + " is " + age;
+
+answer.innerHTML = myName + " is " + age;
